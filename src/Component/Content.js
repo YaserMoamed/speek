ab4899ca6e00d0cea42937380aa10573e26c3f4b
YaserMoamd <yaser_mohamed@outlook.sa>
2020-02-28 16:21:32 +0100

    secode edition

--- src/Component/Content.js
+++ src/Component/Content.js
@@ -1,70 +1,43 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import '../scss/result.css'
-class Content extends Component {
-    constructor(props){
-         super(props)
-         this.state = {
-             organicResults: [],
-             searchResult : ''
-         }
-     }
-     //method
-     
-     componentDidMount() {
-        axios.get('https://api.apify.com/v2/datasets/ZRRzkyMtvqKCA76fH/items?format=json&fields=searchQuery,organicResults&unwind=organicResults')
-        .then(res => res.data)
-        .then(data => this.setState({organicResults: data}))
-        .catch(error => console.log("catch the error " + error));
-     }
-   
-     render() {
-         return (
-             <div className="result">
-             {this.state.organicResults.map((result,index) => (
-                 <div key={index}>
-                 <h1>{result.title}</h1>
-                  <span> {result.url} </span >
-                  <p>{result.description}</p>
-                 </div>
-             ))}
-             </div>
-         );
- }
-}
- export default Content;
+import React, { useState, useEffect } from "react";
+import Speech from "./Speech";
+import "./styles.css";
+import Speak from "./Speech";
 
+const App = () => {
+  const [RelatedTopics, setRelatedTopics] = useState([]);
+  const [query, setQuery] = useState("");
+  const fetchResults = query => {
+    if (query !== "") {
+      fetch(`https://api.duckduckgo.com/?q=${query}&format=json`)
+        .then(res => res.json())
+        .then(data => setRelatedTopics(data.RelatedTopics));
+    }
+  };
+  const handleClick = e => {
+    setQuery(e.target.value);
+  };
+  //componentDidMount
+  useEffect(() => fetchResults(query), [query]);
 
+  return (
+    <div className="Search">
+      <h1>Search something...</h1>
+      <input className="box" type="text" placeholder="Enter text...." />
+      <button onClick={e => handleClick()}>Search</button>
+      <ul>
+        {RelatedTopics.map((hit, index) => (
+          <div>
+            <h4 onClick={e => Speak(hit.Text)}>{hit.Text}</h4>
+            <a href={hit.FirstURL}>
+              <span>{hit.FirstURL}</span>
+            </a>
+            <p>{hit.Result}</p>
+          </div>
+          //<QuoteItem key={index} quote={hit.Text} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default App;
