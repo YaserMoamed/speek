251adc887d8b0b8e4973a20dc9af62e6fd08a2aa
YaserMoamd <yaser_mohamed@outlook.sa>
2020-03-04 17:44:13 +0100

    new update

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <div className="footer">
-    <a href="https://github.com/YaserMoamed/readtext" target="_blank" rel="noopener">
+    <a href="#" target="_blank" rel="noopener">
       Speek Project
     </a>
   </div>
